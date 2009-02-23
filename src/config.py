aa489f8c7c7802eaaaf33c7fad843cf59586ded9
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-02-23 13:42:23 +0000

    - added database path

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Mon Feb 23 14:45:48 2009 on havoc
-# update count: 28
+# Last modified Mon Feb 23 14:52:02 2009 on havoc
+# update count: 29
 # -*- coding:  utf-8 -*-
 
 import ConfigParser
@@ -15,4 +15,5 @@ class dmsconfig:
         self.repopath = conf.get("Path", "repository")
         self.repourl = "file://" + self.repopath
         self.workpath = conf.get("Path", "workspace")
+        self.dbpath = conf.get("Path", "database")
         self.doctypes = list(conf.get("Document", "type").split())
