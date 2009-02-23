67bf00f698cd083c7ab74341b80aa0e6dd25b5c1
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-02-23 13:49:23 +0000

    - converted creatdb to a function

--- src/createdb.py
+++ src/createdb.py
@@ -1,33 +1,38 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Sat Sep  6 22:57:39 2008 on violator
-# update count: 65
+# Last modified Mon Feb 23 14:58:49 2009 on havoc
+# update count: 73
+# -*- coding:  utf-8 -*-
 
-# create database
 from pysqlite2 import dbapi2 as sqlite
 
-# Create a connection to the database file
-con = sqlite.connect("repodb")
+"""
+create database. Foŕ now a simple sqlite2 database is used
+"""
 
-# Get a Cursor object that operates in the context of Connection con:
-cur = con.cursor()
+def creatdb(dbpath):
+    # Create a connection to the database file
+    con = sqlite.connect(dbpath)
+    
+    # Get a Cursor object that operates in the context of Connection con:
+    cur = con.cursor()
 
-# Create schema and test data
-#cur.execute("create table filelist(fileid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, filename TEXT)")
-#cur.execute("create table revlist(revnum INTEGER PRIMARY KEY, id INTEGER, FOREIGN KEY(id) REFERENCES filelist(fileid), logtext TEXT)")
+    # Create schema and test data
+    #cur.execute("create table filelist(fileid INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, filename TEXT)")
+    #cur.execute("create table revlist(revnum INTEGER PRIMARY KEY, id INTEGER, FOREIGN KEY(id) REFERENCES filelist(fileid), logtext TEXT)")
 
-# Create the simpliest table 
-cur.execute("create table revlist(revnum INTEGER PRIMARY KEY,filename, logtext TEXT)")
+    # Create the simpliest table 
+    cur.execute("create table revlist(revnum INTEGER PRIMARY KEY,filename, logtext TEXT)")
 
-#cur.execute("insert into filelist(filename) values ('file1.cpp')")
-#cur.execute("insert into filelist(filename) values ('file3.cpp')")
-#cur.execute("insert into filelist(filename) values ('file8.cpp')")
+    #cur.execute("insert into filelist(filename) values ('file1.cpp')")  
+    #cur.execute("insert into filelist(filename) values ('file3.cpp')")
+    #cur.execute("insert into filelist(filename) values ('file8.cpp')")
 
-cur.execute("insert into revlist(revnum,filename,author,logtext) values (1,'file8.cpp','initial commit')")
+    cur.execute("insert into revlist(revnum,filename,author,logtext) values (1,'file8.cpp','initial commit')")
 
 
 #cur.execute("select * from filelist")
-cur.execute("select * from revlist")
+    cur.execute("select * from revlist")
 
 print cur.fetchall()
 
