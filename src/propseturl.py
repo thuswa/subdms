89dc3df63337eebbd6a835daabc7c9d42955b587
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-06 11:59:08 +0000

    added test function propseturl

---
+++ src/propseturl.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# $Id: config.py 53 2009-03-01 21:34:36Z albert.thuswaldner $
+# Last modified Fri Mar  6 12:58:31 2009 on havoc
+# update count: 53
+# -*- coding:  utf-8 -*-
+
+from svn import fs, repos, core
+
+def propset(docfileurl, propname, propvalue):
+    # Get repository object
+    repository = repos.open(root_path) 
+
+    # Get repo filesystem pointer
+    fs_ptr = repos.fs(repository)
+    
+    # Get youngest revision
+    youngest_revision_number = fs.youngest_rev(fs_ptr)
+
+    # begin transaction
+    txn = fs.begin_txn(fs_ptr, youngest_revision)
+    txn_root = fs.txn_root(txn)
+
+    # change property on node
+    fs.change_node_prop(txn_root, docfileurl, propname, propvalue)
+    
