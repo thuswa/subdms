16fa067e92396a538bf48bb599b91ab905c05233
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-11 18:44:50 +0000

    changed to use lowlib method const_docinrepopath

--- trunk/src/propseturl.py
+++ trunk/src/propseturl.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Wed Mar 11 19:29:27 2009 on violator
-# update count: 89
+# Last modified Wed Mar 11 19:42:30 2009 on violator
+# update count: 94
 # -*- coding:  utf-8 -*-
 
 from svn import fs, repos, core
-import string
 
 import lowlib
 
@@ -31,9 +30,9 @@ def propset(docnamelist, propname, propvalue):
     txn = fs.begin_txn(fs_ptr, youngest_revision_number)
     txn_root = fs.txn_root(txn)
 
-    path=docs.const_docfileurl(docnamelist).split(conf.repopath)[1]
     # change property on node
-    fs.change_node_prop(txn_root,path,propname, propvalue)
+    fs.change_node_prop(txn_root, docs.const_docinrepopath(docnamelist), \
+                        propname, propvalue)
     fs.commit_txn(txn)
 
 
