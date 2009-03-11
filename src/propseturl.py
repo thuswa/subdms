f75d1521fbb13e2917af9df116dc9b8324839d31
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-11 18:30:33 +0000

    changed the includes to point to lowlib classes

--- src/propseturl.py
+++ src/propseturl.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Fri Mar  6 23:17:47 2009 on violator
-# update count: 81
+# Last modified Wed Mar 11 19:29:27 2009 on violator
+# update count: 89
 # -*- coding:  utf-8 -*-
 
 from svn import fs, repos, core
 import string
-import config
 
-conf = config.dmsconfig()
+import lowlib
+
+conf = lowlib.dmsconfig()
+docs = lowlib.docname()
 
 def propset(docnamelist, propname, propvalue):
     """
-    Own-rolled propset function that operates directly on te repo
+    Own-rolled propset function that operates directly on the repo
     However it requires direct filesystem access to the repo.
     """
 
@@ -29,23 +31,9 @@ def propset(docnamelist, propname, propvalue):
     txn = fs.begin_txn(fs_ptr, youngest_revision_number)
     txn_root = fs.txn_root(txn)
 
-    path=__const_docfileurl(docnamelist).split(conf.repopath)[1]
+    path=docs.const_docfileurl(docnamelist).split(conf.repopath)[1]
     # change property on node
     fs.change_node_prop(txn_root,path,propname, propvalue)
     fs.commit_txn(txn)
 
 
-def __const_docname(docnamelist):
-   """ Construct the document file name. """
-   return string.join(docnamelist[:-1],'-')+'.'+docnamelist[-1:].pop()
-
-def __const_docurl(docnamelist):
-   """ Construct the document url. """
-   docurllist=[conf.trunkurl]
-   docurllist.extend(docnamelist[:-1])
-   return string.join(docurllist, '/')
-    
-def __const_docfileurl(docnamelist):
-   """ Construct the document file url. """
-   return string.join([__const_docurl(docnamelist), \
-                       __const_docname(docnamelist)], '/')
