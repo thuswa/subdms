f08427dd62c0e790a28a875f8ed097cfb0290a24
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-12 14:49:40 +0000

    - added documentDialog tp client.py (not completed)

--- trunk/src/client.py
+++ trunk/src/client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Thu Mar 12 00:53:41 2009 on violator
-# update count: 210
+# Last modified Thu Mar 12 14:14:04 2009 on havoc
+# update count: 222
+# -*- coding:  utf-8 -*-
 
 import sys
 from PyQt4 import QtGui
@@ -15,7 +16,6 @@ from createdocumentui import Ui_New_Document_Dialog
 from createprojui import Ui_New_Project_Dialog
 from mainwindow import Ui_MainWindow
 
-
 docs = lowlib.docname()
 db = database.sqlitedb()
 
@@ -25,19 +25,24 @@ class ClientUi(QtGui.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.projdialog = projectDialog()
+        self.docdialog = documentDialog()
         
         # Set column width on list object
         self.ui.documentlist.setColumnWidth(0, 120)
         self.ui.documentlist.setColumnWidth(1, 400)
         self.ui.documentlist.setColumnWidth(2, 100)
+
+        # Connect buttons 
         self.connect(self.ui.new_project_button, QtCore.SIGNAL('clicked()'), \
                      self.projdialog.show)
+        self.connect(self.ui.new_document_button, QtCore.SIGNAL('clicked()'), \
+                     self.docdialog.show)
         self.connect(self.ui.list_documents_button, \
                      QtCore.SIGNAL('clicked()'), self.setdocumentlist)
 
     def setdocumentlist(self):
         n = 0
-        for doc in db.getall():
+        for doc in db.getalldocs():
             docname = QtGui.QTableWidgetItem(docs.const_docname(list(doc[1:5])))
             title = QtGui.QTableWidgetItem(doc[5])
             status = QtGui.QTableWidgetItem(doc[7])
@@ -57,6 +62,22 @@ class projectDialog(QtGui.QDialog):
     def okaction(self):
         frontend.createproject(unicode(self.ui.Project_name.text()))
         self.close()
+
+
+class documentDialog(QtGui.QDialog):
+    def __init__(self, parent=None):
+        QtGui.QDialog.__init__(self, parent)
+        self.ui = Ui_New_Document_Dialog()
+        self.ui.setupUi(self)
+        self.connect(self.ui.New_Document_Confirm, \ 
+                     QtCore.SIGNAL("accepted()"), self.okaction)
+        
+    def okaction(self):
+        title = unicode(self.ui.Document_title.text())
+        
+        frontend.createdocument()
+        self.close()
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
