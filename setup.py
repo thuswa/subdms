9c000197cbb979fc2f99b2b9a99088e5a3e1ec9d
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-13 21:16:54 +0000

    updated package files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Fri Mar 13 20:08:41 2009 on violator
-# update count: 46
+# Last modified Fri Mar 13 22:07:38 2009 on violator
+# update count: 57
 
 from distutils.core import setup
 
@@ -15,7 +15,8 @@ setup(name='subdms',
       """,
       url='http://subdms.googlecode.com',
       packages=['subdms'],
-      package_dir = {'subdms' : 'subdms/lib' },
+#      package_data = {'' : './subdms.cfg' },
+#      scripts = ['subdms']
       platforms='any',
       requires=['pysvn', 'pyqt4', 'pysqlite3', 'svn']
       )
