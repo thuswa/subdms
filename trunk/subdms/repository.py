cec60c86991e1c93a110a7aff3f82be4b7daae7c
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-13 23:34:24 +0000

    property 'svn:keywords' set on file

---
+++ trunk/subdms/repository.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# $Id$
+# Last modified Sat Mar 14 00:28:41 2009 on violator
+# update count: 134
+# -*- coding:  utf-8 -*-
+
+import os
+import pysvn
+import shutil
+import string
+import subprocess 
+
+import lowlevel
+
+class repository:
+    def __init__(self):    
+        self.client = pysvn.Client()
+        self.conf = lowlevel.config()
+        
+    def createrepo(self):
+        """ create repsitory and layout """
+        subprocess.call(['svnadmin','create', self.conf.repopath])
+        self.client.mkdir(self.conf.trunkurl, "create trunk directory",1)
+        self.client.mkdir(self.conf.tagsurl, "create trunk directory",1)
+        self.client.mkdir(self.conf.tmplurl, "create templates directory",1)
+
+    def installhooks(self):
+        """ Install hooks in repository """
+        revhook='post-commit'
+        revhookpath=os.path.join(self.conf.hookspath, revhook)
+        # Copy hooks to dir in repository and set to executable
+        shutil.copyfile(os.path.abspath('lowlib.py'), \
+                        os.path.join(self.conf.hookspath, 'lowlib.py')) #fixme
+        shutil.copyfile(os.path.abspath('../subdms.cfg'), \
+                        os.path.join(self.conf.repopath, 'subdms.cfg')) #fixme
+        shutil.copyfile(os.path.abspath(revhook), revhookpath)
+        os.chmod(revhookpath,0755)
+        
+    def installtemplates(self):
+        """ Install templates in repository """
+        tmplpath=os.path.join(self.conf.workpath,'templates')
+        txtfilepath=os.path.join(tmplpath, self.conf.tmpltxt.split('/')[1])
+        #fixme
+        
+        # Check out templates dir
+        self.client.checkout(self.conf.tmplurl, tmplpath)
+        
+        # Add templates to dir
+        shutil.copyfile(os.path.abspath(self.conf.tmpltxt), txtfilepath)
+        self.client.add(txtfilepath)
+
+        # Commit templates
+        self.client.checkin(tmplpath, "installing templates")
+   
+        # Remove template dir from workspace
+        shutil.rmtree(tmplpath)
+
+
