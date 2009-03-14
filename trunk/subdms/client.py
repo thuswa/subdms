d3eb9344fd00f6adb8d0ca39c6e044dc829f8ccf
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-03-14 21:25:40 +0000

    updated file headers

--- trunk/subdms/client.py
+++ trunk/subdms/client.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Sat Mar 14 00:36:55 2009 on violator
-# update count: 291
+# Last modified Sat Mar 14 22:17:14 2009 on violator
+# update count: 294
 # -*- coding:  utf-8 -*-
+#
+# subdms - A document management system based on subversion.
+# Copyright (C) 2009  Albert Thuswaldner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt4 import QtGui
 from PyQt4 import QtCore
