c78f226abadd9388799b3a51b2fe75307de673d7
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-04-16 21:04:57 +0000

    included commit.ui in generateui scripts

--- tools/generateui.bat
+++ tools/generateui.bat
@@ -1,11 +1,12 @@
 @echo off
 REM $Id: generateui 206 2009-03-19 23:29:29Z albert.thuswaldner $
-REM Last modified Mon Apr 13 16:41:50 2009 on violator
-REM Update count: 107
+REM Last modified Thu Apr 16 23:03:35 2009 on violator
+REM Update count: 108
 REM
 REM Created 2009-03-04, Albert Thuswaldner
 
 pyuic4 ../ui/about.ui > ../subdms/aboutui.py
+pyuic4 ../ui/commit.ui > ../subdms/commitui.py
 pyuic4 ../ui/createdocument.ui > ../subdms/createdocumentui.py
 pyuic4 ../ui/createproj.ui > ../subdms/createprojui.py
 pyuic4 ../ui/documentinfo.ui > ../subdms/documentinfoui.py
