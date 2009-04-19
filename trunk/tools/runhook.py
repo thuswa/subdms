733eac866e34a1851393cae2f5b14f1833a31927
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-04-19 22:28:17 +0000

    property 'svn:keywords' and 'svn:executable' set on file

---
+++ trunk/tools/runhook.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# $Id$
+# Last modified Mon Apr 20 00:27:48 2009 on violator
+# update count: 128
+# -*- coding:  utf-8 -*-
+
+import os
+
+from subdms import lowlevel
+
+conf = lowlevel.config()
+
