8f8fb95384d87b99235afb07bb7fd2c364477fd7
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-04-19 22:24:16 +0000

    added headings for database dumps in debugging tool

--- trunk/tools/dbdump.py
+++ trunk/tools/dbdump.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Mon Apr 20 00:16:57 2009 on violator
-# update count: 117
+# Last modified Mon Apr 20 00:22:31 2009 on violator
+# update count: 122
 # -*- coding:  utf-8 -*-
 
 from subdms import database
@@ -9,8 +9,12 @@ from subdms import database
 db = database.sqlitedb()
 
 # Dump database
+print "Document list:"
 print db.getalldocs()
+print "Template list:"
 print db.getalltmpls()
+print "Project list:"
 print db.getallprojs()
+print "Revision list:"
 print db.getallrev()
 
