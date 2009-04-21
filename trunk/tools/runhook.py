1b52184c01049cda2ee0a189f0c1719cc106d711
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-04-21 10:58:12 +0000

    completed runhook tool

--- trunk/tools/runhook.py
+++ trunk/tools/runhook.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Mon Apr 20 00:27:48 2009 on violator
-# update count: 128
+# Last modified Tue Apr 21 12:57:01 2009 on violator
+# update count: 155
 # -*- coding:  utf-8 -*-
 
-import os
+from optparse import OptionParser
 
 from subdms import lowlevel
 
+cmd = lowlevel.command()
 conf = lowlevel.config()
+link = lowlevel.linkname()
+
+def main():
+  usage = """usage: %prog REV """
+
+  parser = OptionParser(usage=usage)
+ 
+  (opts,(rvn, )) = parser.parse_args()
+
+  repohook = "post-commit"
+  
+  revhookpath = link.const_repohookpath(repohook)
+  repopath = conf.repopath
+
+  cmd.command_output(revhookpath+" "+repopath+" "+rvn)
+if __name__ == "__main__":
+  import sys
+  sys.exit(main())
+
 
