0cd8ebab9fd90fe6af1fbab8b284b6c50660574f
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-04-23 11:51:52 +0000

    bugfixed integration

--- subdms/integration.py
+++ subdms/integration.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Wed Apr 22 21:06:29 2009 on violator
-# update count: 406
+# Last modified Thu Apr 23 01:33:31 2009 on violator
+# update count: 479
 # -*- coding:  utf-8 -*-
 #
 # subdms - A document management system based on subversion.
@@ -24,8 +24,9 @@ import fileinput
 import string
 import re
 
-import lowlevel
 import database
+import epoch
+import lowlevel
 
 """
 Document integration class. 
@@ -36,31 +37,64 @@ class docinteg:
         """ Initialize docinteg """
         self.conf = lowlevel.config()
         self.db = database.sqlitedb()
+        self.dt = epoch.dtime()
         self.link = lowlevel.linkname()
 
     def setallfields(self, docnamelist, doctitle, dockeywords, author, status):
         """ Update all document fields. """
-        
         # Name the document info
         cat = docnamelist[0]
         proj = docnamelist[1]
         issue = docnamelist[-2]
         rdate = ""
         docid = self.link.const_docid(docnamelist)
-        
+
         # Get the document description
         desc = self.db.getprojdesc(cat, proj)
-
+        
         # Create fieldcontents list
         fieldcontents =[doctitle, docid, issue, status, rdate, author, \
-                        proj, desc, keyw]
+                        proj, desc, dockeywords]
         
         # Choose action depending on filetype
         if docnamelist[-1] == "tex": 
             self.texfieldupdate(docnamelist, self.conf.fieldcodes, \
                                     fieldcontents)
 
+    def updatetitle(self, docnamelist, doctitle):
+        """ Update the title field. """
+        fieldcontents = [doctitle]
+        fieldcodes = self.conf.fieldcodes[0:1]
+
+        # Choose action depending on filetype
+        if docnamelist[-1] == "tex": 
+            self.texfieldupdate(docnamelist, fieldcodes, fieldcontents)
         
+    def updatekeywords(self, docnamelist, dockeywords):
+        """ Update the keywords field. """
+        fieldcontents = [dockeywords]
+        fieldcodes = self.conf.fieldcodes[8:9]
+        
+        # Choose action depending on filetype
+        if docnamelist[-1] == "tex": 
+            self.texfieldupdate(docnamelist, fieldcodes, fieldcontents)
+        
+    def releaseupdate(self, docnamelist):
+        """ Update the release date and status field. """
+        fieldcontents = [self.conf.statuslist[4], self.dt.datestamp()]
+        fieldcodes = self.conf.fieldcodes[3:5]
+        # Choose action depending on filetype
+        if docnamelist[-1] == "tex": 
+            self.texfieldupdate(docnamelist, fieldcodes, fieldcontents)
+
+    def obsoleteupdate(self, docnamelist):
+        """ Update the release date and status field. """
+        fieldcontents = [self.conf.statuslist[5], ""] #fixme?
+        fieldcodes = self.conf.fieldcodes[3:5]
+        # Choose action depending on filetype
+        if docnamelist[-1] == "tex": 
+            self.texfieldupdate(docnamelist, fieldcodes, fieldcontents)
+    
     def texfieldpattern(self, fieldcode, fieldcontent=".*"):
         """ return field code string for tex file. """
         return re.compile(".newcommand .."+fieldcode+". ."+fieldcontent+".")
@@ -76,7 +110,13 @@ class docinteg:
             for code, content in map(None, fieldcodes, fieldcontents):
                 fieldptrn = self.texfieldpattern(code)
                 if fieldptrn.match(line):
-                    line = self.texfieldcode(code, content)
+                    line = self.texfieldcode(code, content.replace("\n", r"\\"))
             print line.replace("\n","")
 
-       
+    def dodocinteg(self, docnamelist):
+        """ Check if document integration should be done. """
+        if docnamelist[-1] in self.conf.integtypes \
+               and docnamelist[0] != self.conf.categories[1]:
+            return True
+        else:
+            return False
