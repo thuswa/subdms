55f9cb19f2397efea8d0b5d2c6aa75c14dd5d235
albert.thuswaldner <albert.thuswaldner@1a8bf0da-fed3-11dd-9d1d-173cc1d06e3c>
2009-04-29 12:28:29 +0000

    updated file headers with copyright text

--- tools/repodump.py
+++ tools/repodump.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python
 # $Id$
-# Last modified Wed Apr 29 14:24:57 2009 on violator
-# update count: 136
+# Last modified Wed Apr 29 14:27:30 2009 on violator
+# update count: 137
 # -*- coding:  utf-8 -*-
+#
+# subdms - A document management system based on subversion.
+# Copyright (C) 2009  Albert Thuswaldner
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from subdms import lowlevel
 
